ffbb67dbdfe4df812a142389843877562ff90192
Andrew Pang <andrew.y.pang@gmail.com>
2019-10-13 23:00:49 -0700

    starter code

--- dist/js-andrew/sketch.js
+++ dist/js-andrew/sketch.js
@@ -7,24 +7,3 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.scrollspy').scrollSpy();
 });
-
-function expandAll() {
-    $(".collapsible-header").addClass("active");
-    $(".collapsible").collapsible({
-        accordion: false
-    });
-
-    console.log("expand all!")
-}
-
-function collapseAll() {
-    $(".collapsible-header").removeClass(function () {
-        return "active";
-    });
-    $(".collapsible").collapsible({
-        accordion: true
-    });
-    $(".collapsible").collapsible({
-        accordion: false
-    });
-}
